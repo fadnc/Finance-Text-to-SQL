5457ff5fd3509cae00f1be4c7bdda0e88793136b
Fadhil Muhammed N C <ncfadhil@gmail.com>
2026-03-01 23:53:04 +0530

    LoRA adapter configuration upated

---
+++ src/training/lora_config.py
@@ -0,0 +1,15 @@
+from peft import LoraConfig, get_peft_model
+
+def apply_lora(model):
+    
+    peft_config = LoraConfig(
+        r=16,
+        lora_alpha=32,
+        target_modules=["q_proj", "v_proj"],
+        lora_dropout=0.05,
+        bias="none",
+        task_type="CAUSAL_LM"
+    )
+    
+    model = get_peft_model(model, peft_config)
+    return model
